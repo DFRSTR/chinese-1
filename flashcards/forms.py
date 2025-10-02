79282b3d3e29df545ebb89a0dc3775fb3b6ad20a
DFRSTR <foresterofficial@mail.ru>
2025-10-02 20:18:43 +0300

    Update forms.py

--- flashcards/forms.py
+++ flashcards/forms.py
@@ -39,10 +39,28 @@ class CardForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Инициализация choices для character динамически в JavaScript, поэтому оставляем пустым
-        self.fields['character'].choices = []
+        category = None
+        # Найти категорию в POST-данных или initial
+        if 'data' in kwargs:
+            category = kwargs['data'].get('category')
+        elif 'initial' in kwargs:
+            category = kwargs['initial'].get('category')
+        if category in ['HSK1', 'HSK2', 'HSK3']:
+            from .hsk_data import HSK1_CHARACTERS
+            from .hsk2_data import HSK2_CHARACTERS
+            from .hsk3_data import HSK3_CHARACTERS
+            hsk_map = {
+                'HSK1': HSK1_CHARACTERS,
+                'HSK2': HSK2_CHARACTERS,
+                'HSK3': HSK3_CHARACTERS,
+            }
+            chars = hsk_map[category]
+            self.fields['character'].choices = [(char, char) for char in chars]
+        else:
+            self.fields['character'].choices = []
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
+
         fields = ['username', 'password1', 'password2']
